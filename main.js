4015b803f0dea4ceb080e531b8911c3c03e91ad2
aamnaashraf501@gmail.com <aamnaashraf501@gmail.com>
2024-04-18 15:10:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.red("*** Welcome to Word counter app!***\n"));
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: (chalk.yellow("Please enter your sentence to count the words!")),
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(chalk.green(words));
+console.log(chalk.red(`Your sentence word count is: ${words.length}`));
